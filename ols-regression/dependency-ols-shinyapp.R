16b1f05ef9fbe18e4982f6dd63e6dbd69bf448bb
mgang2 <49343610+mgang2@users.noreply.github.com>
2020-12-19 12:08:22 +0530

    k

---
+++ ols-regression/dependency-ols-shinyapp.R
@@ -0,0 +1,10 @@
+
+if(!require("shiny")) {install.packages("shiny")}
+if(!require("pastecs")){install.packages("pastecs")}
+if(!require("RColorBrewer")){install.packages("RColorBrewer")}
+if(!require("Hmisc")){install.packages("Hmisc")}
+if(!require("ggplot2")){install.packages("ggplot2")}
+if(!require("reshape2")){install.packages("reshape2")}
+if (!require("corrplot")) {install.packages("corrplot")}
+if (!require("PerformanceAnalytics")) {install.packages("PerformanceAnalytics")}
+
