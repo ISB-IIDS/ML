708f1695b76d049bd78de4c7012fcf85bd0e590d
mgang2 <49343610+mgang2@users.noreply.github.com>
2021-02-26 13:09:47 +0530

    k

--- binary-logit/server.R
+++ binary-logit/server.R
@@ -11,6 +11,7 @@ if (!require("corrplot")) {install.packages("corrplot")}
 if (!require("ROCR")) {install.packages("ROCR")}
 if (!require("caret")) {install.packages("caret")}
 if (!require("Rfast")) {install.packages("Rfast")}
+if (!require("e1071")) {install.packages("e1071")}
 
 library(shiny)
 library(pastecs)
@@ -22,6 +23,7 @@ library(corrplot)
 library(ROCR)
 library(caret)
 library(Rfast)
+library(e1071)
 
 # library(gplot)
 
